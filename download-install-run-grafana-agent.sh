ae9c069a10c1a77f7c2503a306bfcca09859ec1f
aengusrooneygrafana <54800912+aengusrooneygrafana@users.noreply.github.com>
2021-04-12 17:10:01 +0200

    Update download-install-run-grafana-agent.sh

--- download-install-run-grafana-agent.sh
+++ download-install-run-grafana-agent.sh
@@ -9,8 +9,10 @@ chmod a+x "$agent-linux-amd64";
 
 # Set up the agent configurations 
 
-USER=$MYUSER
-PASS=$MYPASSWORD
+USER1=$MYUSER1
+PASS1=$MYPASSWORD1
+USER2=$MYUSER2
+PASS2=$MYPASSWORD2
  
 cat <<EOF > ./agent-config.yaml
 integrations:
@@ -18,22 +20,42 @@ integrations:
     enabled: true
   prometheus_remote_write:
     - basic_auth:
-        password: $PASS
-        username: $USER
+        password: $PASS1
+        username: $USER1
       url: https://prometheus-us-central1.grafana.net/api/prom/push
 prometheus:
   configs:
     - name: integrations
       remote_write:
         - basic_auth:
-            password: $PASS 
-            username: $USER 
+            password: $PASS1 
+            username: $USER1 
           url: https://prometheus-us-central1.grafana.net/api/prom/push
   global:
     scrape_interval: 15s
   wal_directory: /tmp/grafana-agent-wal
 server:
   http_listen_port: 12345
+loki:
+  configs:
+  - name: default
+    positions:
+      filename: /tmp/positions.yaml
+    scrape_configs:
+      - job_name: varlogs
+        static_configs:
+          - targets: [localhost]
+            labels:
+              job: varlogs
+              __path__: /var/log/*log
+      - job_name: dmesg
+        static_configs:
+          - targets: [localhost]
+            labels:
+              job: dmesg
+              __path__: /var/log/dmesg
+    clients:
+      - url: https://$USER2:$PASS2@logs-prod-us-central1.grafana.net/loki/api/v1/push
 EOF
 
 # Run the agent
